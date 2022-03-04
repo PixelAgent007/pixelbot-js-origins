e4e38cc8c0cd9705172c339179ee58ad3611143c
PixelAgent007 <oskar.manhart@gmx.de>
2022-03-04 18:43:57 +0100

    Added ticket system

--- Nyxoy4.js
+++ Nyxoy4.js
@@ -1,48 +1,55 @@
 // Imports
-const Power = require("../lib/power.js");
-const Origin = require("../lib/origin.js");
+const Power = require('../lib/power.js');
+const Origin = require('../lib/origin.js');
 
 module.exports = new Origin({
-    name: "Fallen Goddess Nyx",
-    player: "Nyxoy4",
+    name: 'Fallen Goddess Nyx',
+    player: 'Nyxoy4',
     impact: 2,
-    description: "Nyx used to be the Goddess of the Night. Now stripped of her powers, she wanders a mortal plane once thought to be lost.",
-    author: "KitCat#0962",
-    src: "https://github.com/dark-moon-smp/fallen-gods-origins",
+    description:
+        'Nyx used to be the Goddess of the Night. Now stripped of her powers, she wanders a mortal plane once thought to be lost.',
+    author: 'KitCat#0962',
+    src: 'https://github.com/dark-moon-smp/fallen-gods-origins',
     actives: [
         new Power({
-            "name": "Spell of Blindness",
-            "description" : "With your primary key, aflict the entity in your sights with blindness."
-        })
-	],
+            name: 'Spell of Blindness',
+            description:
+                'With your primary key, aflict the entity in your sights with blindness.',
+        }),
+    ],
     pros: [
         new Power({
-            "name": "Remnant Strength",
-            "description": "A bit of your Godly strength still remains, but only while in your element."
+            name: 'Remnant Strength',
+            description:
+                'A bit of your Godly strength still remains, but only while in your element.',
         }),
         new Power({
-            "name": "Adapted Eyes",
-            "description" : "Your natural home is in a cave. As a result, your eyes have adapted to low light levels."
+            name: 'Adapted Eyes',
+            description:
+                'Your natural home is in a cave. As a result, your eyes have adapted to low light levels.',
         }),
         new Power({
-            "name": "Cave Food",
-            "description" : "You gain more from food found in caves than others."
-        })
+            name: 'Cave Food',
+            description: 'You gain more from food found in caves than others.',
+        }),
     ],
     passives: [
         new Power({
-            "name": "Thin Presence",
-            "description" : "You had a thin presence in Godhood and an even thinner presence without it. You appear translucent to others."
-        })
-	],
+            name: 'Thin Presence',
+            description:
+                'You had a thin presence in Godhood and an even thinner presence without it. You appear translucent to others.',
+        }),
+    ],
     cons: [
         new Power({
-            "name": "Sensitive Skin",
-            "description" : "In Godhood, light was your elemental opposite. Turns out, nothing changed. You burn in direct sunlight."
+            name: 'Sensitive Skin',
+            description:
+                'In Godhood, light was your elemental opposite. Turns out, nothing changed. You burn in direct sunlight.',
         }),
         new Power({
-            "name": "Elemental Opposite",
-            "description" : "Grow weak for some time after touching direct sunlight."
-        })
-    ]
+            name: 'Elemental Opposite',
+            description:
+                'Grow weak for some time after touching direct sunlight.',
+        }),
+    ],
 });
--- PixelAgent007.js
+++ PixelAgent007.js
@@ -1,40 +1,41 @@
 // Imports
-const Power = require("../lib/power.js");
-const Origin = require("../lib/origin.js");
+const Power = require('../lib/power.js');
+const Origin = require('../lib/origin.js');
 
 module.exports = new Origin({
-    name: "Phantom",
-    player: "PixelAgent007",
+    name: 'Phantom',
+    player: 'PixelAgent007',
     impact: 3,
-    description: "Is it a plane? Is it a bird? No, it's a fucking phantom clipping through the ground. You all know what this is.",
-    author: "Apace",
-    src: "",
+    description:
+        "Is it a plane? Is it a bird? No, it's a fucking phantom clipping through the ground. You all know what this is.",
+    author: 'Apace',
+    src: '',
     actives: [
         new Power({
-            "name": "Phantomize",
-            "description": "When pressing your primary, you can walk through all blocks except obsidian. Whilst in this state, your hunger drains twice as fast."
-        })
+            name: 'Phantomize',
+            description:
+                'When pressing your primary, you can walk through all blocks except obsidian. Whilst in this state, your hunger drains twice as fast.',
+        }),
     ],
-    pros: [
-    ],
+    pros: [],
     passives: [
         new Power({
-            "name": "Translucent",
-            "description" : "You are slightly translucent."
-        })
+            name: 'Translucent',
+            description: 'You are slightly translucent.',
+        }),
     ],
     cons: [
         new Power({
-            "name": "League of Legends Pro",
-            "description": "You burn in the sun, except when phantomized."
+            name: 'League of Legends Pro',
+            description: 'You burn in the sun, except when phantomized.',
         }),
         new Power({
-            "name": "Frail",
-            "description": "You have 6 hearts."
+            name: 'Frail',
+            description: 'You have 6 hearts.',
         }),
         new Power({
-            "name": "Fast Metabolism",
-            "description": "You need to eat more."
-        })
-    ]
+            name: 'Fast Metabolism',
+            description: 'You need to eat more.',
+        }),
+    ],
 });
